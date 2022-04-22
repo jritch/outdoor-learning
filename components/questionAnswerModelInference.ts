e824ec870a932dc1fba8ff669ddbc4b79d261544
Pranav Deshpande <pranavcd@fb.com>
2022-04-21 19:08:23 -0500

    Modify the logic of the q&a model inference component to use a Tokenizer which improves the model responses for different questions.

--- components/questionAnswerModelInference.ts
+++ components/questionAnswerModelInference.ts
@@ -1,20 +1,22 @@
-import {MobileModel} from 'react-native-pytorch-core';
+import {MobileModel, torch, text} from 'react-native-pytorch-core';
 
-export default async function findAnswer(text: string, question: string) {
+const spec = require('../assets/specs/bert_model_spec.json');
+
+export default async function findAnswer(textString: string, question: string) {
+  const {WordPieceTokenizer} = text;
+  const tokenizer = new WordPieceTokenizer({
+    vocab: spec.vocabulary_bert,
+  });
   const model =
     'https://github.com/pytorch/live/releases/download/v0.1.0/bert_qa.ptl';
-  var answer = undefined;
-  var modelRunMetrics = undefined;
-  const inputText = `[CLS] ${question} [SEP] ${text} [SEP]`;
-  if (text != null && question != null) {
-    const {
-      result: {answer: ans},
-      metrics: m,
-    } = await MobileModel.execute(model, {text: inputText});
-    if (ans != null) {
-      answer = ans;
-      modelRunMetrics = m;
-    }
-  }
-  return {text: answer, metrics: modelRunMetrics};
+  const inputText = `[CLS] ${question} [SEP] ${textString} [SEP]`;
+  const arr = tokenizer.encode(inputText);
+  const t = torch.tensor([arr], {dtype: torch.int});
+  const modelPath = await MobileModel.download(model); // TODO: Revisit this, cache the model download.
+  const nlpModel = await torch.jit._loadForMobile(modelPath);
+  const output = await nlpModel.forward(t);
+  const startId = output.toGenericDict().start_logits.toTensor().argmax(); // TODO: change to argmax().item() later.
+  const endId = output.toGenericDict().end_logits.toTensor().argmax();
+  const res = tokenizer.decode(arr.slice(startId, endId + 1));
+  return {text: res};
 }
