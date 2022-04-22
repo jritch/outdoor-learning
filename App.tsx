6b55a0c9b48632f49c97224420addd280035056d
Justin Haaheim <jhaa@fb.com>
2022-04-21 18:18:09 -0700

    Add a check for ptl core lib at app startup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,26 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import React from 'react';
+import React, {useEffect} from 'react';
+
+function checkForPytorchCoreLib(): boolean {
+  try {
+    const torch = require('react-native-pytorch-core').torch;
+    const tensor = torch.rand([2, 3]);
+    const isAvailable = tensor?.data != null;
+    console.log(
+      'react-native-pytorch-core available:',
+      isAvailable,
+      tensor?.data,
+    );
+    return isAvailable;
+  } catch (e) {
+    console.log('react-native-pytorch-core unavailable:', e);
+    return false;
+  }
+}
+
+checkForPytorchCoreLib();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
