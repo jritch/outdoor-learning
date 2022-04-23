3a4d16f5bdd264ba2dd903098ddd32aeda6a933c
Pranav Deshpande <pranavcd@fb.com>
2022-04-22 21:47:26 -0500

    Add a component for a Bubble which acts like a banner to display messages.

---
+++ components/Bubble.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+type Props = {
+  text: string; // text to be rendered in the bubble
+};
+
+export default function Bubble(props: Props) {
+  const textToBeRendered = props.text;
+
+  return (
+    <View style={styles.bubble}>
+      <Text style={styles.bubbleText}>
+        {textToBeRendered}
+      </Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  bubble: {
+    height: 40,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'background: rgba(18, 18, 18, 1)',
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 11,
+    paddingBottom: 11,
+  },
+  bubbleText: {
+    color: '#ffffff',
+    fontSize: 14,
+    marginBottom: 2,
+    textTransform: 'capitalize',
+  },
+});
