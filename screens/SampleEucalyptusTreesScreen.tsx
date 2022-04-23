0072a5627faa428cd41c83ad30dac34ed362b62a
Pranav Deshpande <pranavcd@fb.com>
2022-04-22 21:57:52 -0500

    Modify the sample eucalyptus images screen.

--- screens/SampleEucalyptusTreesScreen.tsx
+++ screens/SampleEucalyptusTreesScreen.tsx
@@ -1,30 +1,36 @@
 import * as React from 'react';
-import { ImageBackground, Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Image, ImageBackground, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export default function SampleEucalyptusTreesScreen() {
   return (
-    <View style={styles.mainContainer}>
-      <View>
-        <Text style={styles.textSection}>
-          Try using the shape of the leaves to help you identify eucalyptus.
-        </Text>
-      </View>
-      <View style={styles.imageSection}>
-        <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: "https://reactjs.org/logo-og.png"}} resizeMode="cover" style={styles.image} />
-      </View>
-      <View style={styles.textSection}>
-        <Text style={styles.textSection}>
-          You might also notice that eucalyptus has a distinctive bark pattern. See if you can use the bark and the leaves to identify eucalyptus.
-        </Text>
-      </View>
-      <View style={styles.imageSection}>
-        <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: "https://reactjs.org/logo-og.png"}} resizeMode="cover" style={styles.image} />
+    <View style={styles.wrapper}>
+      <View style={styles.mainContainer}>
+        <View>
+          <Text style={styles.textSection}>
+            Try using the shape of the leaves to help you identify eucalyptus.
+          </Text>
+        </View>
+        <View style={styles.imageSection}>
+          <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: "https://reactjs.org/logo-og.png"}} resizeMode="cover" style={styles.image} />
+        </View>
+        <View style={styles.textSection}>
+          <Text style={styles.textSection}>
+            You might also notice that eucalyptus has a distinctive bark pattern. See if you can use the bark and the leaves to identify eucalyptus.
+          </Text>
+        </View>
+        <View style={styles.imageSection}>
+          <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: "https://reactjs.org/logo-og.png"}} resizeMode="cover" style={styles.image} />
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#121212'
+  },
   mainContainer: {
     marginLeft: 24,
     marginRight: 24,
