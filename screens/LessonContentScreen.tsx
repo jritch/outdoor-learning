e813031c27e819d8b489c814db5fb31f597b3c37
Justin Haaheim <jhaa@fb.com>
2022-04-27 15:27:36 -0700

    Lint warn on console logs; remove logs in lesson content components

--- screens/LessonContentScreen.tsx
+++ screens/LessonContentScreen.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import {
-  ImageBackground,
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import type {RootStackParamList} from '../types';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Dimensions} from 'react-native';
 import {EucalyptusLesson} from '../lesson_content/EucalyptusLesson';
 import InformationalComponent from './lesson_elements/InformationalComponent';
 
@@ -16,15 +9,10 @@ export default function LessonContentScreen({
   navigation,
   route,
 }: NativeStackScreenProps<RootStackParamList, 'LessonContentScreen'>) {
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
-  console.log({windowWidth, windowHeight});
-
   const elementId = route.params?.elementId ?? 0;
   const totalElements = EucalyptusLesson.elements.length;
 
   const element = EucalyptusLesson.elements[elementId];
-  console.log('Element', element);
 
   let reactElement = null;
 
