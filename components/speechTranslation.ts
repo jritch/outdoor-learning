1711e7587e613e6914bb0616755342c744401bd8
Pranav Deshpande <pranavcd@fb.com>
2022-05-31 01:56:31 -0500

    Migrate the audio model to use JSI instead of previous pack/unpack method

--- components/speechTranslation.ts
+++ components/speechTranslation.ts
@@ -1,17 +1,29 @@
-import {Audio, MobileModel} from 'react-native-pytorch-core';
+import {Audio, media, Tensor, torch} from 'react-native-pytorch-core';
+import ModelCache from './ModelCache';
 
 export default async function translate(audio: Audio) {
-  const model =
-    'https://github.com/pytorch/live/releases/download/v0.1.0/wav2vec2.ptl';
-  var translatedText = undefined;
-  var modelRunMetrics = undefined;
+  const MODEL_KEY = 'wav2vec';
   if (audio != null) {
-    const {
-      result: {answer},
-      metrics: m,
-    } = await MobileModel.execute(model, {audio});
-    translatedText = answer;
-    modelRunMetrics = m;
+    const modelPath = await ModelCache.getModelPath(MODEL_KEY);
+
+    // Convert the audio data to a blob
+    const blob = media.toBlob(audio);
+
+    // Get a tensor of shorts (int16) for the audio data
+    const tensor = torch.fromBlob(blob, [1, blob.size / 2], {
+      dtype: 'int16',
+    });
+
+    // Convert the tensor to a float tensor since the Wav2Vec model requires the input
+    // tensor in float32 format
+    const floatTensor = tensor.to({dtype: 'float32'});
+
+    // Load the model
+    const speechModel = await torch.jit._loadForMobile(modelPath);
+
+    // Run inference with the given tensor
+    const result = await speechModel.forward<Tensor, string>(floatTensor);
+    return {text: result.toLowerCase()};
   }
-  return {text: translatedText.toLowerCase(), metrics: modelRunMetrics};
+  return {text: ''};
 }
