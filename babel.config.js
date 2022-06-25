114090f9e46378ee0862834ad48f506138bc65fe
Justin Haaheim <jhaa@fb.com>
2022-06-24 17:24:48 -0700

    Enable absolute paths for project modules in components, assets and constants

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './components',
+            // modules: './src/modules',
+            // lib: './src/lib',
+            // types: './src/types',
+            constants: './constants',
+          },
+        },
+      ],
+    ],
   };
 };
