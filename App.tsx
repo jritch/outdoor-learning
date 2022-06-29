b6558bd51641ddc0e2bb6f4c8b1c22edc84b7b67
Justin Haaheim <jhaa@fb.com>
2022-06-28 23:27:36 -0700

    Show loading screen while preloading models

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import 'expo-dev-client';
-import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import * as ModelCache from './components/ModelCache';
 import ModelURLs from './constants/ModelURLs';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 
 function checkForPytorchCoreLib(): boolean {
   try {
@@ -29,12 +29,26 @@ function checkForPytorchCoreLib(): boolean {
 checkForPytorchCoreLib();
 
 export default function App() {
+  const [isModelPreloadingComplete, setIsModelPreloadingComplete] =
+    useState<boolean>(false);
+  const [appError, setAppError] = useState<Error | unknown | null>(null);
+
   // Cache all models required by the app
   useEffect(() => {
     console.log('Preloading all models...');
-    Promise.all(
-      Object.values(ModelURLs).map(async url => ModelCache.preloadModel(url)),
-    );
+    async function preloadModels() {
+      await Promise.all(
+        Object.values(ModelURLs).map(async url => ModelCache.preloadModel(url)),
+      );
+      setIsModelPreloadingComplete(true);
+    }
+
+    try {
+      preloadModels();
+    } catch (e) {
+      console.error('Error when preloading models:', e);
+      setAppError(e);
+    }
   }, []);
 
   const isLoadingComplete = useCachedResources();
@@ -43,14 +57,48 @@ export default function App() {
   // const colorScheme = useColorScheme();
   const colorScheme = 'dark';
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  if (appError != null) {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <SafeAreaView style={styles.messageContainer}>
+        <Text style={styles.messageText}>
+          The app has encountered an error. Please try again.
+        </Text>
+      </SafeAreaView>
     );
   }
+
+  if (!isLoadingComplete || !isModelPreloadingComplete) {
+    return (
+      <SafeAreaView style={styles.messageContainer}>
+        <View style={styles.spinner}>
+          <ActivityIndicator color="white" size="large" />
+        </View>
+        <Text style={styles.messageText}>Loading resources</Text>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Navigation colorScheme={colorScheme} />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  messageText: {
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 1)',
+    textAlign: 'center',
+  },
+  messageContainer: {
+    flex: 1,
+    padding: 40,
+    justifyContent: 'center',
+    backgroundColor: '#121212',
+  },
+  spinner: {
+    margin: 16,
+  },
+});
